0bc20db17280f44063aa433787bd146165baed9f
Pavel Bitiukov <pavleg.bityukov@gmail.com>
2023-10-20 13:41:30 +0100

    Added Dockerfile and docker-compose to run the app with postgresql

--- 01_shared_database_shared_schema/app/shared_schema/settings.py
+++ 01_shared_database_shared_schema/app/shared_schema/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,11 +75,14 @@ WSGI_APPLICATION = 'shared_schema.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shared_schema',
+        'USER': 'shared_schema',
+        'PASSWORD': 'shared_schema',
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': '5432',
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
